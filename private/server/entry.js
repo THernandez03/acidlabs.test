539f2f82cef32b17a6f569feee3d6208b11b8f8d
Tomás Hernández <tomas.hernandez03@gmail.com>
2017-08-09 02:01:29 -0400

    Add Redis cache of the requested data

--- private/server/entry.js
+++ private/server/entry.js
@@ -1,6 +1,8 @@
 import http from 'http';
 import { readFileSync } from 'fs';
 import socketIO from 'socket.io';
+import redis from 'redis';
+import moment from 'moment';
 
 const index = readFileSync('./public/server/views/index.html', 'utf8');
 
@@ -9,7 +11,40 @@ const server = http.createServer((req, res) => {
   res.end(index);
 });
 const io = socketIO(server);
+const client = redis.createClient();
 
-server.listen(3000, () => {
-  console.log('Server listening on localhost:3000');
+const requestData = () => (
+  new Promise((resolve, reject) => {
+    if(Math.random() < 0.1){
+      throw new Error('How unfortunate! The API Request Failed');
+    }
+    http.request({
+      host: 'finance.google.com',
+      path: '/finance/info?client=ig&q=AAPL,ABC,MSFT,TSLA,F',
+    }, (res) => {
+      let str = '';
+      res.on('data', (chunk) => {
+        str += chunk;
+      });
+      res.on('end', () => {
+        resolve(JSON.parse(str.slice(4)));
+      });
+    }).end();
+  })
+)
+
+const withCache = (request) => async () => {
+  (await request()).forEach((stock) => {
+    const { t: stockName } = stock;
+    client.hset(stockName, moment().unix(), JSON.stringify(stock))
+  })
+}
+
+const requestWithCache = withCache(requestData);
+
+client.on('connect', () => {
+  server.listen(3000, async () => {
+    console.log('Server listening on localhost:3000');
+    requestWithCache();
+  });
 });
